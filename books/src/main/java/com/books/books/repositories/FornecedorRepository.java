c0dc28d6b29d59ae126a83497f704c9424a7ee4c
Matheus Goulart <goulartmatheus611@gmail.com>
2026-03-19 10:34:43 -0300

    Fornecedor Repository

---
+++ books/src/main/java/com/books/books/repositories/FornecedorRepository.java
@@ -0,0 +1,7 @@
+package com.books.books.repositories;
+
+import com.books.books.models.FornecedorModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FornecedorRepository extends JpaRepository<FornecedorModel, Long> {
+}
